58d1d4200291a1df8fa9027bb2565c05a7c94213
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-02-22 10:01:30 +0100

    project setup

---
+++ rsvp-app/app/_components/RSVPForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RSVPForm = () => {
+  return <div>RSVPForm</div>;
+};
+
+export default RSVPForm;
---
+++ rsvp-app/app/page.tsx
@@ -0,0 +1,9 @@
+import RSVPForm from './_components/RSVPForm';
+
+export default function Home() {
+  return (
+    <main className='h-screen flex items-center justify-center'>
+      <RSVPForm />
+    </main>
+  );
+}
